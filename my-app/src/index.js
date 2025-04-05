fc82d9186457e7173af1d2614c6f1bfeecb6c4fb
하경 <hamhagyeong@hakyeong-ui-MacBookAir.local>
2025-04-06 01:24:11 +0900

    4주차 과제 업로드

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Accommodate from './chapter_07/Accommodate'
-
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <AttendanceBook/>
   </React.StrictMode>
 );
 
